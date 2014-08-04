2e67e10ef61f6a275edf862382d6a5877e687491
bmcmahen <ben.mcmahen@gmail.com>
2014-08-04 11:48:07 -0600

    first

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = function(url){
+  var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
+  var match = url.match(regExp);
+  if (match && match[2].length==11){
+    return match[2];
+  }
+};
